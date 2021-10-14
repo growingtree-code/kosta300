8a7961aa9458286f838af4badc54001661046bbb
jne6161 <jne8181@gmail.com>
2021-10-14 12:25:00 +0900

    실기 시험문제

---
+++ Kosta300project/src/kosta/ClassSoojebi.java
@@ -0,0 +1,29 @@
+package kosta;
+
+class SuperObj{
+	public void show() {
+		print();
+	}
+	public void print() {
+		print();
+		System.out.println("Super");
+	}
+}
+
+class SubObj extends SuperObj{
+	public void show() {
+		super.print();
+	}
+	public void print() {
+		System.out.println("Sub");
+	}
+}
+
+
+public class ClassSoojebi {
+	public static void main(String[] args) {
+		SuperObj s = new SubObj();
+		s.show();
+	}
+
+}
